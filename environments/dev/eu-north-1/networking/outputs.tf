22c11416b1de6c00c60f0ae84e1a455f5ca2cd77
Arielgeller1995 <ariel.geller11@gmail.com>
2025-08-31 16:43:26 +0300

    Create outputs.tf

---
+++ environments/dev/eu-north-1/networking/outputs.tf
@@ -0,0 +1,14 @@
+output "vpc_id"             
+{ 
+value = module.vpc.vpc_id 
+}
+
+output "private_subnet_ids"
+{
+value = module.vpc.private_subnets
+}
+
+output "webapp_sg_id" 
+{ 
+value = aws_security_group.webapp_sg.id 
+}

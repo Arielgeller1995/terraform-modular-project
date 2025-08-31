e3c9d7beb844646ed047114641588385af08c418
Arielgeller1995 <ariel.geller11@gmail.com>
2025-08-31 16:41:42 +0300

    Update main.tf

---
+++ environments/dev/eu-north-1/networking/main.tf
@@ -0,0 +1,9 @@
+module "vpc" {
+  source             = "../../../../../../modules/networking/vpc"
+  name               = local.name
+  vpc_cidr           = local.vpc_cidr
+  azs                = local.azs
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  tags               = local.tags
+}

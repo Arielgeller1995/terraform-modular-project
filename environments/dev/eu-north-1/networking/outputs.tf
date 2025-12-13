9dd4ad2f91ca9ddd278df23a25d46cdd2a8c398a
Ariel Geller <arielgeller@icloud.com>
2025-12-13 13:58:39 +0200

    Fix VPC nat gateway output

--- environments/dev/eu-north-1/networking/outputs.tf
+++ environments/dev/eu-north-1/networking/outputs.tf
@@ -2,10 +2,3 @@ output "vpc_id" {
   value = module.vpc.vpc_id
 }
 
-output "private_subnet_ids" {
-  value = module.vpc.private_subnets
-}
-
-output "webapp_sg_id" {
-  value = aws_security_group.webapp_sg.id
-}

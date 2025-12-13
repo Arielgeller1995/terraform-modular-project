cd31c530b0737a5958838cb112d4922069fc0664
Ariel Geller <arielgeller@icloud.com>
2025-12-13 13:35:17 +0200

    Fix TFLint issues for networking and kubernetes placeholder

--- environments/dev/eu-north-1/kubernetes/main.tf
+++ environments/dev/eu-north-1/kubernetes/main.tf
@@ -1,14 +1,4 @@
-module "eks" {
-  source = "../../../../../../modules/kubernetes/eks"
+# environments/dev/eu-north-1/kubernetes/main.tf
+# Placeholder – נבנה את מודול ה-EKS בשלב הבא
 
-  cluster_version = var.cluster_version
-  ami_type        = var.ami_type
-  instance_types  = var.instance_types
-  min_size        = var.min_size
-  desired_size    = var.desired_size
-  max_size        = var.max_size
-
-  vpc_id             = data.terraform_remote_state.networking.outputs.vpc_id
-  private_subnet_ids = data.terraform_remote_state.networking.outputs.private_subnet_ids
-  security_group_ids = [data.terraform_remote_state.networking.outputs.webapp_sg_id]
-}
+# כרגע אין משאבים, זה רק כדי שהפייפליין יעבור ללא שגיאות.
--- environments/dev/eu-north-1/kubernetes/versions.tf
+++ environments/dev/eu-north-1/kubernetes/versions.tf
@@ -1,11 +1,10 @@
-# versions.tf - Terraform and Provider Version Requirements
-
 terraform {
-  required_version = "~> 1.9.0"
+  required_version = ">= 1.9.5"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 6.0"
+      version = "~> 5.0"
     }
   }
 }

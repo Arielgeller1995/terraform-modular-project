489aabd655b46425dc1857b1c918e209c6053777
Ariel Geller <arielgeller@icloud.com>
2025-12-13 14:36:11 +0200

    Define locals for VPC module in networking

--- modules/networking/vpc/main.tf
+++ modules/networking/vpc/main.tf
@@ -1,4 +1,16 @@
-# vpc.tf - VPC Module for EKS Networking
+# modules/networking/vpc/main.tf
+
+locals {
+  name     = "dev-networking-vpc"
+  vpc_cidr = "10.0.0.0/16"
+
+  azs = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  tags = {
+    Environment = "dev"
+    Project     = "terraform-modular"
+  }
+}
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
@@ -7,8 +19,8 @@ module "vpc" {
   name = local.name
   cidr = local.vpc_cidr #the entire VPC
 
-  azs             = local.azs                                                  #availability zones
-  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)] #LOOP-FOR
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
   public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
 
   enable_nat_gateway   = true
@@ -29,36 +41,4 @@ module "vpc" {
 
 data "aws_availability_zones" "available" {
   state = "available"
-}
-# security_groups.tf - Security Groups for Worker Nodes
-
-resource "aws_security_group" "webapp_sg" {
-  name        = "terraform-webapp-sg"
-  description = "Allow SSH and HTTP traffic to EKS nodes"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    from_port   = 22 #SSH
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] #open for the world
-  }
-
-  ingress {
-    from_port   = 80 #HTTP
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] #open for the world
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1" #all protocols
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = { #the tags are to identify
-    Name = "terraform-webapp-sg"
-  }
 }

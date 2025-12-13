07baf8dae34443f90685435f7d379904caa52ed3
Ariel Geller <arielgeller@icloud.com>
2025-12-13 13:31:36 +0200

    remove region from locals.tf

--- environments/dev/eu-north-1/networking/locals.tf
+++ environments/dev/eu-north-1/networking/locals.tf
@@ -1,6 +1,5 @@
 locals {
   name     = var.cluster_name
-  region   = var.aws_region
   azs      = data.aws_availability_zones.available.names
   vpc_cidr = "10.0.0.0/16"
   tags     = { Environment = "dev", Project = "eks" }
